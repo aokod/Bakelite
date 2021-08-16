67c2e94763df69ef7910b7d624fb7c32852d5f95
grntbg <13023526+grntbg@users.noreply.github.com>
2021-08-16 10:32:39 -0500

    Initial push of skin contents

---
+++ README.md
@@ -0,0 +1,6 @@
+### Bakelite
+A glossy skin with rounded corners for your eso forum.
+
+[![License](https://img.shields.io/github/license/grntbg/Bakelite)]()
+[![Version](https://img.shields.io/github/v/release/grntbg/Bakelite?include_prereleases)]()
+[![PHP Version Support](https://img.shields.io/badge/php-%5E5.6.4-blue)]()
---
+++ skin.php
@@ -0,0 +1,39 @@
+<?php
+// Bakelite/skin.php
+// Bakelite skin file.
+
+if (!defined("IN_ESO")) exit;
+
+class Bakelite extends Skin {
+
+var $name = "Bakelite";
+var $version = "1.0";
+var $author = "eso";
+var $numberOfColors = 27;
+
+// Add stylesheets and a favicon to the page header.
+function init()
+{
+	global $config;
+	$this->eso->addCSS("skins/{$config["skin"]}/styles.css");
+	$this->eso->addCSS("skins/{$config["skin"]}/ie6.css", "ie6");
+	$this->eso->addCSS("skins/{$config["skin"]}/ie7.css", "ie7");
+	$this->eso->addToHead("<link rel='shortcut icon' type='image/ico' href='skins/{$config["skin"]}/favicon.ico'/>");
+}
+
+// Generate button HTML.
+function button($attributes)
+{
+	$class = $id = $style = ""; $attr = " type='submit'";
+	foreach ($attributes as $k => $v) {
+		if ($k == "class") $class = " $v";
+		elseif ($k == "id") $id = " id='$v'";
+		elseif ($k == "style") $style = " style='$v'";
+		else $attr .= " $k='$v'";
+	}
+	return "<span class='button$class'$id$style><input$attr/></span>";
+}
+
+}
+
+?>
